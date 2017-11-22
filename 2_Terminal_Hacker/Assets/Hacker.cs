b273fc20ba3785537ba5a10525ca607107870a86
David McKoy <dmckoy85@gmail.com>
2017-11-21 20:35:24 -0500

    initial commit

---
+++ 2_Terminal_Hacker/Assets/Hacker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ShowMainMenu("Welcome David");
+	}
+
+	void ShowMainMenu (string greeting) {
+		Terminal.ClearScreen();
+		Terminal.WriteLine(greeting);	
+		Terminal.WriteLine("What would you like to hack into?");	
+		Terminal.WriteLine("Press 1 - Neigbhors WIFI");	
+		Terminal.WriteLine("Press 2 - Google");	
+		Terminal.WriteLine("Press 3 - NSA");	
+		Terminal.WriteLine("Enter your selection:");	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
